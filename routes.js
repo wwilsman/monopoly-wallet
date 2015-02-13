98b7fa5bed38e15dd2d0cd618e3281030a67d546
Wil Wilsman <wil.wilsman@ymail.com>
2015-02-13 13:52:20 -0500

    Remove Game caching and fix tests

--- routes.js
+++ routes.js
@@ -1,13 +1,9 @@
 var MonopolyGame = require('./lib/main');
-var M = new MonopolyGame(JSON.parse(require('fs').readFileSync('example.json')));
 
 module.exports = function(app, io) {
 
   app.get('/', function(req, res) {
-    res.render('index', { title: 'Monopoly', test: M.id });
+    res.render('index', { title: 'Monopoly' });
   });
-
-  app.get('/:id', function(req, res) {
-    res.send(MonopolyGame.get(req.params.id));
-  });
+  
 };
--- test/game.js
+++ test/game.js
@@ -1,62 +1,76 @@
 var assert = require('assert');
-var _ = require('../lib/helpers');
 var MonopolyGame = require('../lib/main');
 
-var exconfig = JSON.parse(require('fs').readFileSync('./example.json'));
-
 describe('Game', function() {
-  var config
-
-  beforeEach(function() {
-    config = _.extend({}, exconfig);
-  });
-
-  it('should create a new game and cache it', function() {
-    var M = new MonopolyGame;
-
-    assert.ok(M);
-    assert.equal(M, MonopolyGame.get(M.id));
+  it('should create a new game', function() {
+    assert.ok(new MonopolyGame);
   });
 
   it('should create player and property instances', function() {
-    var M = new MonopolyGame(config);
+    var M = new MonopolyGame({
+      players: [{ name: '1' }],
+      properties: [{
+        name: '1',
+        group: '1', 
+        costs: {
+          price: 1,
+          build: 1,
+          price: 100
+        }
+      }]
+    });
 
-    assert.ok(M.Player.get('player 1') instanceof M.Player);
-    assert.ok(M.Property.get('Oriental Avenue') instanceof M.Property);
+    assert.ok(M.Player.get('1') instanceof M.Player);
+    assert.ok(M.Property.get('1') instanceof M.Property);
   });
 
   it('should start players with custom balance', function() {
-    config.startBalance = 1;
+    var M = new MonopolyGame({
+      players: [{ name: '1' }],
+      startBalance: 1
+    });
 
-    var M = new MonopolyGame(config);
-
-    assert.equal(1, M.Player.get('player 1').balance);
+    assert.equal(1, M.Player.get('1').balance);
   });
 
   it('should start bank with custom balance', function() {
-    config.bankBalance = 1;
-
-    var M = new MonopolyGame(config);
+    var M = new MonopolyGame({
+      bankBalance: 1
+    });
 
     assert.equal(1, M.Bank.balance);
   });
 
   it('should have custom building limits', function() {
-    config.availableHouses = 1;
-    config.availableHotels = 1;
-
-    var M = new MonopolyGame(config);
+    var M = new MonopolyGame({
+      availableHouses: 1,
+      availableHotels: 1
+    });
 
     assert.equal(1, M.availableHouses);
     assert.equal(1, M.availableHotels);
   });
 
   it('should have custom rates', function() {
-    config.rates = { mortgage: 0, unmortgage: 1, building: 0 };
-    config.properties[0].costs.price = 100;
+    var M = new MonopolyGame({
+      properties: [{
+        name: '1',
+        group: '1', 
+        costs: {
+          price: 1,
+          build: 1,
+          price: 100
+        }
+      }],
 
-    var M = new MonopolyGame(config);
-    var prop = M.Property.get(config.properties[0].name);
+      rates: {
+        mortgage: 0,
+        unmortgage: 1,
+        building: 0
+      }
+    });
+
+    var prop = M.Property.get('1');
 
     assert.equal(0, prop.values.mortgage);
     assert.equal(prop.costs.price, prop.costs.mortgage);
