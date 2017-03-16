802b3fff2d9c38d1996dcc061a55f80872543094
Wil Wilsman <wilw@spiceworks.com>
2017-03-16 17:30:37 -0500

    Show property won at auction

--- app/views/Player/PlayerContainer.js
+++ app/views/Player/PlayerContainer.js
@@ -1,17 +1,25 @@
 import { connect } from 'react-redux'
 import { payBank, collectMoney } from '../../actions/game'
 import { getCurrentPlayer, createGetProperties } from '../../selectors/player'
+import { createGetProperty } from '../../selectors/game'
 
 import Player from './Player'
 
 const PlayerContainer = connect(
   () => {
     const getProperties = createGetProperties()
+    const getProperty = createGetProperty()
 
-    return (state) => ({
-      player: getCurrentPlayer(state),
-      properties: getProperties(state, state.player)
-    })
+    return (state) => {
+      const { notice } = state.game
+      const property = notice.meta && notice.meta.property
+
+      return {
+        player: getCurrentPlayer(state),
+        properties: getProperties(state, state.player),
+        showProperty: property && getProperty(state, property)
+      }
+    }
   }, {
     payBank,
     collectMoney
