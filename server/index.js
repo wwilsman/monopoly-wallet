2cf0c097d691002aaa7efafab41c63dc3324a087
Wil Wilsman <wil@frontside.io>
2017-06-22 21:25:24 -0500

    Use mongodb to persist games

--- server/index.js
+++ server/index.js
@@ -1,25 +1,55 @@
 /* eslint no-console: "off" */
 import express from 'express';
 import io from 'socket.io';
+import { MongoClient } from 'mongodb';
 
 import GameRoom from './room';
 
 // environment specific variables
 const ENV = {
   port: process.env.PORT || 3000,
-  host: process.env.HOST || 'localhost'
+  host: process.env.HOST || 'localhost',
+
+  mongodb: {
+    uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/monopoly-wallet'
+  }
 };
 
 // setup express
 const app = express();
 
 // in-memory persistence layer for testing
-const tmp = {};
+if (process.env.NODE_ENV === 'testing') {
+  const tmp = {};
 
-GameRoom.persist({
-  find: (id) => !tmp[id] ? Promise.reject('Game not found') : Promise.resolve(tmp[id]),
-  save: (doc) => Promise.resolve(tmp[doc.id] = doc)
-});
+  GameRoom.persist({
+    find: (id) => tmp[id] ? Promise.resolve(tmp[id]) :
+      Promise.reject('Game not found'),
+    save: (doc) => Promise.resolve(tmp[doc.id] = doc)
+  });
+
+// mongodb persistence
+} else {
+  MongoClient.connect(ENV.mongodb.uri)
+    .then((db) => GameRoom.persist({
+      find: (id) => db.collection('games')
+        .findOne({ _id: id }).then((game) => (
+          new Promise((resolve, reject) => (
+            game ? resolve(game) : reject('Game not found')
+          ))
+        )),
+      save: (game) => db.collection('games')
+        .findOneAndUpdate({ _id: game.id }, { $set: game }, {
+          returnOriginal: false,
+          upsert: true
+        }).then((doc) => new Promise((resolve, reject) => {
+          if (!doc.ok) return reject('Game not found');
+          // eslint-disable-next-line no-unused-vars
+          const { _id, ...game } = doc.value;
+          return resolve(game);
+        }))
+    }));
+}
 
 // start the server
 const server = app.listen(ENV.port, () => {
