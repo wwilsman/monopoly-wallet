f183c38df85b5e0a558d35867cf2c54921c47802
Wil Wilsman <wil@frontside.io>
2017-06-24 17:19:29 -0500

    Remove testing environment

--- server/index.js
+++ server/index.js
@@ -19,44 +19,30 @@ const ENV = {
 // setup express
 const app = express();
 
-// in-memory persistence layer for testing
-if (process.env.NODE_ENV === 'testing') {
-  const tmp = {};
-
-  GameRoom.persist({
-    find: (id) => tmp[id] ? Promise.resolve(tmp[id]) :
-      Promise.reject('Game not found'),
-    save: (doc) => Promise.resolve(tmp[doc.id] = doc)
-  });
-
 // mongodb persistence
-} else {
-  MongoClient.connect(ENV.mongodb.uri)
-    .then((db) => GameRoom.persist({
-      find: (id) => db.collection('games')
-        .findOne({ _id: id }).then((game) => (
-          new Promise((resolve, reject) => (
-            game ? resolve(game) : reject('Game not found')
-          ))
-        )),
-      save: (game) => db.collection('games')
-        .findOneAndUpdate({ _id: game.id }, { $set: game }, {
-          returnOriginal: false,
-          upsert: true
-        }).then((doc) => new Promise((resolve, reject) => {
-          if (!doc.ok) return reject('Game not found');
-          // eslint-disable-next-line no-unused-vars
-          const { _id, ...game } = doc.value;
-          return resolve(game);
-        }))
-    }));
-}
+MongoClient.connect(ENV.mongodb.uri)
+  .then((db) => GameRoom.persist({
+    find: (id) => db.collection('games')
+      .findOne({ _id: id }).then((game) => (
+        new Promise((resolve, reject) => (
+          game ? resolve(game) : reject('Game not found')
+        ))
+      )),
+    save: (game) => db.collection('games')
+      .findOneAndUpdate({ _id: game.id }, { $set: game }, {
+        returnOriginal: false,
+        upsert: true
+      }).then((doc) => new Promise((resolve, reject) => {
+        if (!doc.ok) return reject('Game not found');
+        // eslint-disable-next-line no-unused-vars
+        const { _id, ...game } = doc.value;
+        return resolve(game);
+      }))
+  }));
 
 // start the server
 const server = app.listen(ENV.port, () => {
-  if (process.env.NODE_ENV !== 'testing') {
-    console.log(`Now listening at http://${ENV.host}:${ENV.port}`);
-  }
+  console.log(`Now listening at http://${ENV.host}:${ENV.port}`);
 });
 
 // listen for socket connections
