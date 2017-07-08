81195f2434e92e5ff8f0f3d05b5ce90726b20362
Wil Wilsman <wil@frontside.io>
2017-07-08 15:26:58 -0500

    Add more tests for the welcome screen

--- test/app/acceptance/welcome-test.js
+++ test/app/acceptance/welcome-test.js
@@ -9,12 +9,51 @@ import {
 describe('App: welcome screen', function() {
   setupAppForAcceptanceTesting();
 
+  it('should render the welcome screen', function() {
+    expect(this.$.find('Welcome')).to.exist;
+  });
+
   it('should display the app name', function() {
     expect(this.$.find('h1')).to.have.text('MonopolyWallet');
+  });
+
+  it('should display a new game button', function() {
+    expect(this.$.find('Button').at(0)).to.have.text('New Game');
+  });
+
+  it('should display a join game button', function() {
+    expect(this.$.find('Button').at(1)).to.have.text('Join Game');
   });
 
   it('should redirect back to welcome on 404', function() {
     visit('/404-not-found');
     expect(this.location.pathname).to.equal('/');
+  });
+
+  describe('clicking the new game button', function() {
+    beforeEach(function() {
+      this.$.find('Button').at(0).simulate('click');
+    });
+
+    it('should create a new game', function() {
+      expect(this.app.store.getState()).to.have.deep.property('game.id');
+    });
+
+    it('should go to the join game screen for the new game', function() {
+      const { game: { id } } = this.app.store.getState();
+      expect(this.location.pathname).to.equal(`/${id}/join`);
+      expect(this.$.find('JoinGame')).to.exist;
+    });
+  });
+
+  describe('clicking the join game button', function() {
+    beforeEach(function() {
+      this.$.find('Button').at(1).simulate('click');
+    });
+
+    it('should go to the join game screen', function() {
+      expect(this.location.pathname).to.equal('/join');
+      expect(this.$.find('JoinGame')).to.exist;
+    });
   });
 });
