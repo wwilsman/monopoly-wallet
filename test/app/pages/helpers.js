6ec0ef2d84a3de6aff7b3694b359f639d401ab13
Wil Wilsman <wil@frontside.io>
2017-07-30 12:53:39 -0500

    Use slimmer trigger-change helper

--- test/app/pages/helpers.js
+++ test/app/pages/helpers.js
@@ -1,5 +1,4 @@
 import $ from 'jquery';
-import triggerChange from 'react-trigger-change';
 
 export function click(selector) {
   $(selector).get(0).click();
@@ -10,9 +9,36 @@ export function clickable(selector) {
 }
 
 export function fill(selector, value) {
-  $(selector).val(value).each(triggerChange);
+  $(selector).val(value)
+    .each((i, el) => triggerChange(el));
 }
 
 export function fillable(selector) {
   return (value) => fill(selector, value);
+}
+
+function triggerChange(node) {
+  const initialValue = node.value;
+
+  // cache artificial value property descriptor.
+  const descriptor = Object.getOwnPropertyDescriptor(node, 'value');
+
+  // update inputValueTracking cached value
+  node.value = initialValue + '#';
+
+  // remove artificial value property
+  delete node.value;
+
+  // restore initial value to trigger event with it
+  node.value = initialValue;
+
+  // dispatch input event
+  const event = document.createEvent('HTMLEvents');
+  event.initEvent('input', true, false);
+  node.dispatchEvent(event);
+
+  // restore artificial value property descriptor
+  if (descriptor) {
+    Object.defineProperty(node, 'value', descriptor);
+  }
 }
