e575d2c6775d617aaf083da8086ea4c35d62175e
Wil Wilsman <wil@frontside.io>
2017-08-09 20:49:19 -0500

    Clean up configuration files

--- karma.conf.js
+++ karma.conf.js
@@ -1,41 +1,24 @@
-const path = require('path');
-const webpackConfig = require('./webpack.config');
-
-delete webpackConfig.entry;
-delete webpackConfig.devServer.hot;
-
 module.exports = function(config) {
   config.set({
     frameworks: ['mocha'],
     reporters: ['mocha'],
-    singleRun: true,
 
     browsers: ['Chrome'],
 
     files: [
-      { pattern: 'test/app/acceptance/index.js', watched: false }
+      { pattern: 'test/app/acceptance/index.js', watched: false },
+      { pattern: 'server/themes/classic/icons.svg', watched: false, included: false }
     ],
+
+    proxies: {
+      '/icons/classic.svg': '/base/server/themes/classic/icons.svg'
+    },
 
     preprocessors: {
       'test/app/acceptance/index.js': ['webpack']
     },
 
-    webpack: Object.assign(webpackConfig, {
-      module: {
-        rules: webpackConfig.module.rules.concat([
-          {
-            test: /\.yml/,
-            use: ['json-loader', 'yaml-loader']
-          }
-        ])
-      },
-      resolve: {
-        alias: {
-          'mock-socket': 'mock-socket/src',
-          'socket.io-client': 'mock-socket/socket-io'
-        }
-      }
-    }),
+    webpack: require('./webpack.config'),
 
     webpackMiddleware: {
       stats: 'errors-only'
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
 
-const environment = process.env.NODE_ENV || 'development';
-const env = (conf) => conf[environment];
-
 module.exports = {
   devtool: env({
     development: 'inline-source-map'
@@ -43,64 +40,105 @@ module.exports = {
     ]
   }),
 
+  resolve: {
+    alias: env({
+      test: {
+        'mock-socket': 'mock-socket/src',
+        'socket.io-client': 'mock-socket/socket-io'
+      }
+    })
+  },
+
   module: {
-    rules: [
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader'
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          babelrc: false,
-          presets: [
-            ['env', { modules: false }],
-            'stage-2',
-            'react'
-          ],
-          plugins: env({
-            development: [
-              'react-hot-loader/babel',
-              'transform-decorators-legacy'
+    rules: env({
+      base: [
+        {
+          enforce: "pre",
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'eslint-loader'
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            babelrc: false,
+            presets: [
+              ['env', { modules: false }],
+              'stage-2',
+              'react'
             ],
-            production: [
-              'transform-decorators-legacy'
-            ]
-          })
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              sourceMap: true,
-              importLoaders: 2,
-              localIdentName: '[name]__[local]--[hash:base64:5]'
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins() {
-                return [
-                  require('precss'),
-                  require('autoprefixer')
-                ];
+            plugins: env({
+              base: ['transform-decorators-legacy'],
+              development: ['react-hot-loader/babel']
+            })
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                sourceMap: true,
+                importLoaders: 2,
+                localIdentName: '[name]__[local]--[hash:base64:5]'
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins() {
+                  return [
+                    require('precss'),
+                    require('autoprefixer')
+                  ];
+                }
               }
             }
-          }
-        ]
-      }
-    ]
+          ]
+        }
+      ],
+      test: [
+        {
+          test: /\.yml/,
+          use: ['json-loader', 'yaml-loader']
+        }
+      ]
+    })
   }
+};
+
+/**
+ * Intelligently chooses some configuration based on the current Node
+ * environment. All values can optionally be extended from a provided
+ * base configuration.
+ *
+ * @param {Object} configs - hash of environment specific configurations
+ * @param {Mixed} [configs.base] - base configuration to extend from
+ * @param {Mixed} [configs.development] - "development" only configuration
+ * @param {Mixed} [configs.production] - "production" only configuration
+ * @param {Mixed} [configs.test] - "test" only configuration
+ * @returns {Object} the value of the configuration for a specific
+ * environment; maybe merged with `configs.base`
+ */
+function env(configs) {
+  let conf = configs[process.env.NODE_ENV || 'development'];
+
+  if (configs.base) {
+    if (Array.isArray(configs.base) && Array.isArray(conf)) {
+      conf = configs.base.concat(conf);
+    } else if (typeof configs.base === 'object' && typeof conf === 'object') {
+      conf = Object.assign({}, configs.base, conf);
+    } else {
+      conf = conf || configs.base;
+    }
+  }
+
+  return conf;
 };
