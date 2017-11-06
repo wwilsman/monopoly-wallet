2783870246b560ec3fbbfbdb17ce7fad88e7d7c5
Wil Wilsman <wil@frontside.io>
2017-11-05 21:57:10 -0600

    Add Link component

---
+++ app/ui/link.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { push } from '../actions/router';
+
+@connect(null, { push })
+class Link extends Component {
+  static propTypes = {
+    to: PropTypes.string.isRequired,
+    onClick: PropTypes.func,
+    push: PropTypes.func.isRequired
+  };
+
+  handleClick = (e) => {
+    let { to, onClick, push } = this.props;
+    let bubble = onClick && onClick(e);
+
+    if (bubble !== false && !e.defaultPrevented) {
+      e.preventDefault();
+      push(to);
+    }
+
+    return bubble;
+  };
+
+  render() {
+    // eslint-disable-next-line no-unused-vars
+    let { to, onClick, push, ...props } = this.props;
+
+    return (
+      <a href={to} onClick={this.handleClick} {...props}/>
+    );
+  }
+}
+
+export default Link;
