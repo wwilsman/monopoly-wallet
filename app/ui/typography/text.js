1c27b001f39ad00614ac4b432b993d6def73e6d0
Wil Wilsman <wil@frontside.io>
2017-11-20 02:10:58 -0600

    Add additional text properties

--- app/ui/typography/text.js
+++ app/ui/typography/text.js
@@ -5,18 +5,32 @@ import styles from './typography.css';
 
 const cx = classNames.bind(styles);
 
-const Text = ({ center, error, className, ...props }) => (
+const Text = ({
+  color,
+  center,
+  error,
+  sm,
+  upper,
+  className,
+  ...props
+}) => (
   <span
       className={cx(className, {
+        [`color--${color}`]: !!color,
         'is-center': center,
-        'is-error': error
+        'is-error': error,
+        'is-upper': upper,
+        'size--sm': sm
       })}
       {...props}/>
 );
 
 Text.propTypes = {
+  color: PropTypes.oneOf(['primary', 'secondary']),
   center: PropTypes.bool,
   error: PropTypes.bool,
+  sm: PropTypes.bool,
+  upper: PropTypes.bool,
   className: PropTypes.string
 };
 
