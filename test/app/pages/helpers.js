511f1fc33f72fa982601e6d9487f38f437572199
Wil Wilsman <wil@frontside.io>
2017-11-22 18:27:27 -0600

    Upgrade page helpers to be convergent

--- test/app/pages/helpers.js
+++ test/app/pages/helpers.js
@@ -1,19 +1,84 @@
 import $ from 'jquery';
+import { expect } from 'chai';
+import { convergeOn, convergent } from '../test-helpers';
 
-export function click(selector) {
-  const node = $(selector).get(0);
-  node && node.click();
+/**
+ * A convergent helper for selecting an element using jQuery and
+ * ensuring that the element exists in the DOM
+ * @param {String} selector - jQuery selector
+ * @returns {Promise} a converging promise
+ */
+export function select(selector) {
+  return convergeOn(() => {
+    let $node = $(selector);
+    expect($node).to.exist;
+    return $node;
+  });
 }
 
-export function fill(selector, value) {
-  $(selector).val(value)
-    .each((i, el) => triggerChange(el));
+/**
+ * A convergent click helper that waits for the element to exist and,
+ * by default, asserts that the element was clicked
+ * @param {String} selector - jQuery selector
+ * @param {Function} [assertion] - convergent assertion
+ * @returns {Promise} a converging promise
+ */
+export function click(selector, assertion) {
+  // used for the default assertion
+  let clicked = false;
+  let useDefault = !assertion;
+
+  if (useDefault) {
+    assertion = () => {
+      expect(clicked, `unable to click ${selector}`).to.be.true;
+    };
+  }
+
+  return select(selector).then(($node) => {
+    if (useDefault) {
+      $node.one('click', () => clicked = true);
+    }
+
+    $node.get(0).click();
+  }).then(convergent(assertion));
 }
 
+/**
+ * A convergent fill helper that waits for the input to exist and,
+ * by default, asserts that the value of the input has changed
+ * @param {String} selector - jQuery selector
+ * @param {String} value - value to fill
+ * @param {Function} [assertion] - convergent assertion
+ * @returns {Promise} a converging promise
+ */
+export function fill(selector, value, assertion) {
+  if (!assertion) {
+    assertion = () => {
+      // we don't care about the case
+      let val = ($(selector).val() || '').toLowerCase();
+      expect(val).to.equal(value.toLowerCase());
+    };
+  }
+
+  return select(selector).then(($input) => {
+    $input.val(value).each((i, el) => {
+      triggerChange(el);
+    });
+  }).then(convergent(assertion));
+}
+
+/**
+ * Helper to trigger a react change event
+ *
+ * Works by caching react's custom value property descriptor and
+ * reapplying it after an input event is dispatched on the node
+ *
+ * @param {Node} node - a DOM API Node object
+ */
 function triggerChange(node) {
   const initialValue = node.value;
 
-  // cache artificial value property descriptor.
+  // cache artificial value property descriptor
   const descriptor = Object.getOwnPropertyDescriptor(node, 'value');
 
   // update inputValueTracking cached value
