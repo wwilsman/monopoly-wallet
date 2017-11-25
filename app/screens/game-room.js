18fc3a61872f0f947a9b184e2422eb585df6c8c8
Wil Wilsman <wil@frontside.io>
2017-11-25 17:39:10 -0600

    Give the game room the ability to connect to and join a room

--- app/screens/game-room.js
+++ app/screens/game-room.js
@@ -2,18 +2,30 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import route from './route';
 
+import { connectToGame, joinGame } from '../redux/game';
+
 import { Container, Section } from '../ui/layout';
 import { NavLeft, NavRight } from '../ui/nav';
 import Text from '../ui/typography/text';
 import Toaster from '../ui/toaster';
+import Spinner from '../ui/spinner';
 
 @route(({ app, game }) => ({
-  player: app.player && game.players[app.player]
-}))
+  room: app.room,
+  player: app.player,
+  connected: !!game
+}), {
+  connectToGame,
+  joinGame
+})
 
 class GameRoomScreen extends Component {
   static propTypes = {
+    room: PropTypes.string,
     player: PropTypes.object,
+    connected: PropTypes.bool.isRequired,
+    connectToGame: PropTypes.func.isRequired,
+    joinGame: PropTypes.func.isRequired,
     replace: PropTypes.func.isRequired,
     params: PropTypes.shape({
       room: PropTypes.string.isRequired
@@ -22,34 +34,59 @@ class GameRoomScreen extends Component {
 
   componentWillMount() {
     let {
+      room,
       player,
+      connected,
+      connectToGame,
       params,
       replace
     } = this.props;
 
-    if (!player) {
+    if (!player || room !== params.room) {
       replace(`/${params.room}/join`);
+    } else if (room && !connected) {
+      connectToGame(room);
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    let {
+      player,
+      connected,
+      joinGame
+    } = nextProps;
+
+    // if we are connected, but weren't before,
+    // we should also join the game
+    if (connected && !this.props.connected) {
+      joinGame(player);
     }
   }
 
   render() {
-    let { params } = this.props;
+    let { connected, params } = this.props;
 
     return (
       <Container data-test-game-room>
-        <Section flex="none" row>
-          <NavLeft/>
+        {!connected ? (
+          <Section align="center" justify="center">
+            <Spinner xl/>
+          </Section>
+        ) : [
+          <Section key={0} flex="none" row>
+            <NavLeft/>
 
-          <NavRight>
-            {!!params.room && (
-              <Text sm upper color="secondary" data-test-room-code>
-                {params.room}
-              </Text>
-            )}
-          </NavRight>
-        </Section>
+            <NavRight>
+              {!!params.room && (
+                <Text sm upper color="secondary" data-test-room-code>
+                  {params.room}
+                </Text>
+              )}
+            </NavRight>
+          </Section>,
 
-        <Toaster/>
+          <Toaster key={1}/>
+        ]}
       </Container>
     );
   }
