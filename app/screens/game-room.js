239fa2a36e41402203f5676c179a9a63f2e32b7b
Wil Wilsman <wil@frontside.io>
2017-12-03 18:35:52 -0600

    Auto connect & join a game when persisting states

--- app/screens/game-room.js
+++ app/screens/game-room.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import route from './route';
 
 import { connectToGame, joinGame } from '../redux/game';
+import { load } from '../redux/persist';
 
 import { Container, Section } from '../ui/layout';
 import { NavLeft, NavRight } from '../ui/nav';
@@ -10,11 +11,17 @@ import Text from '../ui/typography/text';
 import Toaster from '../ui/toaster';
 import Spinner from '../ui/spinner';
 
-@route(({ app, game }) => ({
-  room: app.room,
-  player: app.player,
-  connected: !!game
-}), {
+@route(({ app, game, router }, { params }) => {
+  let { location } = router;
+  let persisted = load('app');
+
+  return {
+    room: app.room || persisted.room || params.room,
+    player: app.player || persisted.player || location.state.player,
+    connected: !!game,
+    joined: !!app.player
+  };
+}, {
   connectToGame,
   joinGame
 })
@@ -24,6 +31,7 @@ class GameRoomScreen extends Component {
     room: PropTypes.string,
     player: PropTypes.object,
     connected: PropTypes.bool.isRequired,
+    joined: PropTypes.bool.isRequired,
     connectToGame: PropTypes.func.isRequired,
     joinGame: PropTypes.func.isRequired,
     replace: PropTypes.func.isRequired,
@@ -32,43 +40,51 @@ class GameRoomScreen extends Component {
     }).isRequired
   };
 
+  // when the component updates during the connect event, we lose the
+  // current player in both the local and persisted state. Saving the
+  // initial player prop allows us to reference it later during the
+  // component update hook
+  state = {
+    player: this.props.player
+  };
+
   componentWillMount() {
     let {
       room,
       player,
       connected,
+      joined,
       connectToGame,
+      joinGame,
       params,
       replace
     } = this.props;
 
     if (!player || room !== params.room) {
       replace(`/${params.room}/join`);
-    } else if (room && !connected) {
+    } else if (!connected) {
       connectToGame(room);
+    } else if (!joined) {
+      joinGame(player.name, player.token);
     }
   }
 
   componentWillReceiveProps(nextProps) {
-    let {
-      player,
-      connected,
-      joinGame
-    } = nextProps;
+    let { connected, joinGame } = nextProps;
+    let { player } = this.state;
 
-    // if we are connected, but weren't before,
-    // we should also join the game
+    // if we just connected join the game
     if (connected && !this.props.connected) {
-      joinGame(player);
+      joinGame(player.name, player.token);
     }
   }
 
   render() {
-    let { connected, params } = this.props;
+    let { room, joined } = this.props;
 
     return (
       <Container data-test-game-room>
-        {!connected ? (
+        {!joined ? (
           <Section align="center" justify="center">
             <Spinner xl/>
           </Section>
@@ -77,9 +93,9 @@ class GameRoomScreen extends Component {
             <NavLeft/>
 
             <NavRight>
-              {!!params.room && (
+              {!!room && (
                 <Text sm upper color="secondary" data-test-room-code>
-                  {params.room}
+                  {room}
                 </Text>
               )}
             </NavRight>
