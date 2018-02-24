4fcc872d776d242d8b004ea5c9d8f04265385ad0
Wil Wilsman <wil@frontside.io>
2018-02-24 17:56:58 -0600

    Use redux-deep-diff for keeping game history

--- server/reducers/index.js
+++ server/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import diffable from 'redux-deep-diff';
 
 import bank from './bank';
 import houses from './houses';
@@ -14,14 +15,25 @@ import hydratable from './hydrate';
  * Main reducer for our game
  */
 export default hydratable(
-  combineReducers({
-    bank,
-    houses,
-    hotels,
-    players,
-    properties,
-    auction,
-    trades,
-    notice
-  })
+  diffable(
+    combineReducers({
+      bank,
+      houses,
+      hotels,
+      players,
+      properties,
+      auction,
+      trades,
+      notice
+    }), {
+      // do not diff auctions or trades; we don't need their history
+      prefilter(path, key) {
+        return ['auction', 'trades'].includes(key);
+      },
+      // flatten notice diffs because most properties change at once
+      flatten(path, key) {
+        return key === 'notice';
+      }
+    }
+  )
 );
