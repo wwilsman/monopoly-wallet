41dfa7a097f8ba4e7bed3f87d982401090140d54
Wil Wilsman <wil@wilwilsman.com>
2019-06-20 16:49:02 -0500

    Allow text to render an icon and accept other colors

--- app/src/ui/typography/text.js
+++ app/src/ui/typography/text.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
+
+import Icon from '../icon';
+
 import styles from './typography.css';
 
 const cx = classNames.bind(styles);
 
 Text.propTypes = {
-  color: PropTypes.oneOf(['primary', 'secondary']),
+  color: PropTypes.oneOf([
+    'primary',
+    'secondary',
+    'light',
+    'lighter'
+  ]),
   center: PropTypes.bool,
   error: PropTypes.bool,
   sm: PropTypes.bool,
   xl: PropTypes.bool,
   upper: PropTypes.bool,
-  className: PropTypes.string
+  icon: PropTypes.string,
+  className: PropTypes.string,
+  children: PropTypes.node
 };
 
 export default function Text({
@@ -22,13 +32,16 @@ export default function Text({
   sm,
   xl,
   upper,
+  icon,
   className,
+  children,
   ...props
 }) {
   return (
     <span
       className={cx('root', {
         [`color--${color}`]: !!color,
+        'with-icon': !!icon,
         'is-center': center,
         'is-error': error,
         'is-upper': upper,
@@ -36,6 +49,13 @@ export default function Text({
         'size--xl': xl
       }, className)}
       {...props}
-    />
+    >
+      {!icon ? children : (
+        <>
+          <Icon name={icon} className={styles.icon}/>
+          <span>{children}</span>
+        </>
+      )}
+    </span>
   );
 }
