5a1fbc9ae50c0e9cb50c9949a786efa40cc81534
Wil Wilsman <wil@wilwilsman.com>
2019-07-05 19:17:37 -0500

    Add property to color icons

--- app/src/ui/icon/icon.js
+++ app/src/ui/icon/icon.js
@@ -9,17 +9,30 @@ const cx = classNames.bind(styles);
 Icon.propTypes = {
   name: PropTypes.string.isRequired,
   themed: PropTypes.bool,
+  color: PropTypes.oneOf(['primary', 'secondary', 'alert']),
   className: PropTypes.string
 };
 
-export default function Icon({ name, themed, className, ...props }) {
+export default function Icon({
+  name,
+  themed,
+  color,
+  className,
+  ...props
+}) {
   let url = useSelector(({ app: { theme }, config: { playerTokens } }) => {
     let whitelist = (playerTokens || []).concat(['currency', 'building', 'bank']);
     return (theme && (themed || whitelist.includes(name))) ? `/icons/${theme}.svg` : '/icons.svg';
   });
 
   return (
-    <span title={name} className={cx('root', className)} {...props}>
+    <span
+      title={name}
+      className={cx('root', {
+        [`color--${color}`]: !!color
+      }, className)}
+      {...props}
+    >
       <svg>
         <use xlinkHref={`${url}#${name}`}/>
       </svg>
