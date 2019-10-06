27682bbf040a2c0a68c082fbb4224fc6e3f06d44
Wil Wilsman <wil@wilwilsman.com>
2019-10-05 19:41:23 -0500

    :sparkles: Add player-select to property-transfer-form

--- app/src/game/property-transfer-form.js
+++ app/src/game/property-transfer-form.js
@@ -5,25 +5,34 @@ import { Container, Section } from '../ui/layout';
 import { CurrencyInput } from '../ui/forms';
 import Button from '../ui/button';
 
+import PlayerSelect from './player-select';
 import Property from './property';
 
 PropertyTransferForm.propTypes = {
-  loading: PropTypes.bool.isRequired,
+  players: PropTypes.array,
   property: Property.propTypes.property,
+  loading: PropTypes.bool.isRequired,
   onSubmit: PropTypes.func.isRequired
 };
 
 export default function PropertyTransferForm({
+  players,
   property,
   loading,
   onSubmit
 }) {
-  let [ value, setValue ] = useState(null);
+  let [ price, setPrice ] = useState(null);
+  let [ recipient, setRecipient ] = useState(players?.[0]);
+  let isPurchasing = property.owner === 'bank';
 
   let handleSubmit = useCallback(e => {
     e.preventDefault();
-    onSubmit(value ?? property.price);
-  }, [onSubmit, value]);
+    onSubmit((
+      isPurchasing
+        ? price ?? property.price
+        : recipient.token
+    ));
+  }, [onSubmit, price, recipient]);
 
   return (
     <Container
@@ -32,16 +41,25 @@ export default function PropertyTransferForm({
       data-test-property-transfer-form
     >
       <Section collapse justify="center" align="center">
-        <CurrencyInput
-          label="Enter amount"
-          value={value}
-          defaultValue={property.price}
-          onChange={v => setValue(v)}
-          onBlur={() => !value && setValue(null)}
-          color="secondary"
-          center
-          xl
-        />
+        {isPurchasing ? (
+          <CurrencyInput
+            label="Enter amount"
+            value={price}
+            defaultValue={property.price}
+            onChange={v => setPrice(v)}
+            onBlur={() => !price && setPrice(null)}
+            color="secondary"
+            center
+            xl
+          />
+        ) : (
+          <PlayerSelect
+            label="To:"
+            players={players || []}
+            selected={recipient}
+            onSelect={setRecipient}
+          />
+        )}
       </Section>
 
       <Section row flex={4}>
@@ -54,10 +72,9 @@ export default function PropertyTransferForm({
           type="submit"
           style="primary"
           loading={loading}
-          disabled={loading || value === 0}
-          data-test-join-game-btn
+          disabled={loading || (isPurchasing ? price === 0 : !recipient)}
         >
-          Buy Property
+          {`${isPurchasing ? 'Buy' : 'Transfer'} Property`}
         </Button>
       </Section>
     </Container>
