f10b56c0d475c30ffa3baf26ca94f5e426dc9cb2
Wil Wilsman <wil@wilwilsman.com>
2019-10-17 23:41:07 -0500

    :sparkles: Show bankrupt option on bank screen
    Clicking the button opens a bankrupt form window as a confirmation and way to
    select a beneficiary

--- app/src/screens/bank.js
+++ app/src/screens/bank.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-import { useGame } from '../api';
+import { useGame, useEmit } from '../api';
+import { usePlayers } from '../helpers/hooks';
 
 import { Container, Section } from '../ui/layout';
 import { Text } from '../ui/typography';
 import NavBar from '../ui/nav-bar';
 import Card from '../ui/card';
+import Modal from '../ui/modal';
 
-export default function BankScreen() {
-  let { room } = useGame();
+import BankruptForm from '../game/bankrupt-form';
+
+BankScreen.propTypes = {
+  push: PropTypes.func.isRequired
+};
+
+export default function BankScreen({ push }) {
+  let { room, player } = useGame();
+  let players = usePlayers({ exclude: [player.token] });
+  let [ showBankruptForm, toggleBankruptForm ] = useState(false);
+  let [ bankrupt, bankruptResponse ] = useEmit('player:bankrupt');
+
+  let handleBankrupt = useCallback(beneficiary => {
+    if (!bankruptResponse.pending) bankrupt(beneficiary.token);
+  }, [bankruptResponse.pending, bankrupt]);
+
+  useEffect(() => {
+    if (bankruptResponse.ok) push(`/${room}`);
+  }, [bankruptResponse.ok]);
 
   return (
     <Container data-test-bank>
@@ -31,7 +51,27 @@ export default function BankScreen() {
             Properties
           </Text>
         </Card>
+
+        <Card onClick={() => toggleBankruptForm(true)}>
+          <Text upper icon="currency">
+            Bankrupt
+          </Text>
+        </Card>
       </Section>
+
+      {showBankruptForm && (
+        <Modal
+          title="Bankrupt"
+          titleIcon="currency"
+          onClose={() => toggleBankruptForm(false)}
+        >
+          <BankruptForm
+            players={players}
+            loading={bankruptResponse.pending}
+            onSubmit={handleBankrupt}
+          />
+        </Modal>
+      )}
     </Container>
   );
 }
