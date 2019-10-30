f609a23467375c9fac9d92a046b23e9aaf10ec38
Wil Wilsman <wil@wilwilsman.com>
2019-10-29 20:20:57 -0500

    :recycle: Use div instead of button for card components
    When no linkTo was provided it defaulted to a typeless button; even when onClick
    was not provided.

--- app/src/ui/card/card.js
+++ app/src/ui/card/card.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
 
 import Link from '../link';
 import styles from './card.css';
+
+const cx = classNames.bind(styles);
 
 Card.propTypes = {
   linkTo: PropTypes.any,
@@ -17,12 +20,14 @@ export default function Card({
   className,
   children
 }) {
-  let Component = linkTo ? Link : 'button';
+  let Component = linkTo ? Link : 'div';
   let props = linkTo ? { to: linkTo } : { onClick };
 
   return (
     <Component
-      className={[styles.root, className].filter(Boolean).join(' ')}
+      className={cx('root', {
+        'is-clickable': !!(linkTo || onClick)
+      }, className)}
       data-test-card
       {...props}
     >
