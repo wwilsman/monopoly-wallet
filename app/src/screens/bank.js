1161b6cd1aacae82eaee49cbeff753756e58a4b3
Wil Wilsman <wil@wilwilsman.com>
2020-04-11 15:28:29 -0500

    ✨ Add GameHistory component to Bank screen

--- app/src/screens/bank.js
+++ app/src/screens/bank.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState, useEffect } from 'react';
+import React, { useCallback, useState, useEffect, useMemo } from 'react';
 import PropTypes from 'prop-types';
 
 import { useGame, useEmit } from '../api';
@@ -11,17 +11,22 @@ import Card from '../ui/card';
 import Modal from '../ui/modal';
 
 import BankruptForm from '../game/bankrupt-form';
+import GameHistory from '../game/game-history';
 
 BankScreen.propTypes = {
   push: PropTypes.func.isRequired
 };
 
 export default function BankScreen({ push }) {
-  let { room, player } = useGame();
+  let { room, player, notice, history } = useGame();
   let players = usePlayers({ exclude: [player.token] });
   let { bankrupt: isBankrupt } = usePlayer(player.token);
   let [ showBankruptForm, toggleBankruptForm ] = useState(false);
   let [ bankrupt, bankruptResponse ] = useEmit('player:bankrupt');
+
+  let notices = useMemo(() => (
+    [notice].concat(history.map(({ notice }) => notice)).filter(Boolean)
+  ), [notice, history]);
 
   let handleBankrupt = useCallback(beneficiary => {
     if (!bankruptResponse.pending) bankrupt(beneficiary.token);
@@ -64,6 +69,11 @@ export default function BankScreen({ push }) {
           </Card>
         </Section>
       )}
+
+      <GameHistory
+        player={player}
+        history={notices}
+      />
 
       {showBankruptForm && (
         <Modal
