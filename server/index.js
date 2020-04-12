2df850394a6111176668094a0e666343cfbbc148
Wil Wilsman <wil@wilwilsman.com>
2020-04-12 13:36:05 -0500

    ♻️ Refactor server constants

--- server/index.js
+++ server/index.js
@@ -6,16 +6,12 @@ const WebSocket = require('ws');
 const YAML = require('yamljs');
 
 // env vars and defaults
-const ENV = {
-  env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 3000,
-  host: process.env.HOST || 'localhost',
-
-  mongodb: {
-    uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/monopoly-wallet',
-    options: { useNewUrlParser: true }
-  }
-};
+const {
+  NODE_ENV: ENV = 'development',
+  PORT = 3000,
+  HOST = 'localhost',
+  MONGODB_URI = 'mongodb://localhost:27017/monopoly-wallet'
+} = process.env;
 
 // setup express app
 const app = express();
@@ -27,7 +23,7 @@ app.use('/icons/:theme.svg', (req, res) => {
 });
 
 // setup webpack dev middleware
-if (ENV.env === 'development') {
+if (ENV === 'development') {
   const webpack = require('webpack');
   const webpackDevMiddleware = require('webpack-dev-middleware');
   const webpackHotMiddleware = require('webpack-hot-middleware');
@@ -52,12 +48,12 @@ if (ENV.env === 'development') {
 }
 
 // start the server
-const server = app.listen(ENV.port, () => {
-  console.log(`Now listening at http://${ENV.host}:${ENV.port}`);
+const server = app.listen(PORT, () => {
+  console.log(`Now listening at http://${HOST}:${PORT}`);
 });
 
 // setup the game room
-const GameRoom = require(ENV.env === 'development' ? './src' : './dist').default;
+const GameRoom = require(ENV === 'development' ? './src' : './dist').default;
 const wss = new WebSocket.Server({ server });
 const grm = new GameRoom.Manager({ wss });
 
@@ -70,7 +66,10 @@ grm.use({
 
 // connect mongo
 MongoClient
-  .connect(ENV.mongodb.uri, ENV.mongodb.options)
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(client => {
     let db = client.db().collection('games');
     let resolveGame = ({ _id, ...game } = {}) => !_id
@@ -87,3 +86,50 @@ MongoClient
   .catch(err => {
     console.log('Unable to connect to mongodb: %s', err.name);
   });
+
+// create a game with a specific scenario for development
+if (ENV === 'development') {
+  require('./test/index'); // sets up env for mock helper
+  const { mockGame } = require('./test/helpers/mock-server');
+
+  mockGame.call(grm, {
+    room: 't35tt',
+    players: [{
+      token: 'top-hat',
+      name: 'PLAYER 1'
+    }, {
+      token: 'automobile',
+      name: 'PLAYER 2'
+    }, {
+      token: 'scottish-terrier',
+      name: 'PLAYER 3'
+    }, {
+      token: 'battleship',
+      name: 'PLAYER 4'
+    }],
+    properties: [
+      { id: 'baltic-avenue', owner: 'top-hat', mortgaged: true },
+      { id: 'oriental-avenue', owner: 'automobile' },
+      { id: 'vermont-avenue', owner: 'automobile' },
+      { group: 'orange', owner: 'top-hat', monopoly: true, buildings: 4 },
+      { id: 'new-york-avenue', buildings: 5 },
+      { id: 'indiana-avenue', owner: 'automobile' },
+      { id: 'atlantic-avenue', owner: 'scottish-terrier' },
+      { id: 'ventnor-avenue', owner: 'scottish-terrier' },
+      { id: 'boardwalk', owner: 'top-hat' },
+      { id: 'reading-railroad', owner: 'top-hat' },
+      { id: 'pennsylvania-railroad', owner: 'top-hat' },
+      { id: 'electric-company', owner: 'top-hat' },
+      { id: 'water-works', owner: 'scottish-terrier' }
+    ],
+    notice: { message: 'PLAYER 1 did something', meta: { player: 'top-hat' } },
+    history: [
+      { notice: { message: 'PLAYER 2 did something', meta: { player: 'automobile' } } },
+      { notice: { message: 'PLAYER 1 did something', meta: { player: 'top-hat' } } },
+      { notice: { message: 'PLAYER 3 did something', meta: { player: 'scottish-terrier' } } },
+      { notice: { message: 'PLAYER 4 did something', meta: { player: 'battleship' } } },
+      { notice: { message: 'PLAYER 2 did something', meta: { player: 'automobile' } } },
+      { notice: { message: 'PLAYER 1 did something', meta: { player: 'top-hat' } } }
+    ]
+  });
+}
